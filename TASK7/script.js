130e6b2a496403012c20597a2336547fcb13f610
Mohammed Ansari <112924825+Mohammedansari808@users.noreply.github.com>
2022-09-28 16:30:00 +0530

    Add files via upload

---
+++ TASK7/script.js
@@ -0,0 +1,37 @@
+let xml= new XMLHttpRequest()
+let url ="https://restcountries.com/v2/all"
+
+xml.onload= function (){
+    let data1 = JSON.parse(xml.response)
+       console.log(data1)
+// Get all the countries from the Asia continent /region using the Filter function
+    let asiaregion = data1.filter((ele)=>(ele.region == "Asia")).map((ele)=>ele.name)
+    console.log(asiaregion)
+// Get all the countries with a population of less than 2 lakhs using Filter function
+    let populate = data1.filter((ele)=>ele.population < 200000).map((ele)=>ele.name)
+    console.log(populate)
+// Print the following details name, capital, flag using forEach function
+    let NCF = data1.forEach((ele)=>console.log(`NAME : ${ele.name} CAPITAL : ${ele.capital} FLAG : ${ele.flag}`))
+// Print the total population of countries using reduce function
+    let totalPopu = data1.reduce((acc,cv)=>acc+cv.population,0)
+    console.log(`POPULATION : ${totalPopu}`)
+// Print the country which uses US Dollars as currency.
+let arr;
+let Us = data1.map((ele)=> arr=  ele.currencies)
+for (let keys in Us ){
+    console.log(Us[keys])
+}
+
+
+
+    
+    
+    
+}
+
+
+
+
+
+xml.open("GET",url);
+xml.send()
